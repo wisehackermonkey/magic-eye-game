0d01dd2b188665ef1a00c76f4bbe44a87cec2349
wisehackermonkey <oranbusiness@gmail.com>
2019-03-03 23:03:53 -0800

    array to control grid within the magic eye image canvas

--- sketch.js
+++ sketch.js
@@ -5,12 +5,30 @@
  github.com/wisehackermonkey
  20190303
 */
+let game_board = [];
+
 function setup() {
-	createCanvas(400, 400);
-	background(50);
-	let magicEye = new MagicEye({ el: "magic-eye", width: 500, height: 400 }).render();
+    createCanvas(400, 400);
+    background(50);
+    game_board = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ];
+
+    render_magic_eye(game_board);
 }
 
 function draw() {
-	
+
+}
+
+//draws the game board to a canvas using magice eye image
+function render_magic_eye(depth_map) {
+    depth_map = depth_map || [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ];
+    new MagicEye({el: "magic-eye",depthMap:depth_map, width: 500, height: 400,}).render();
 }
