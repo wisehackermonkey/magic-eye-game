7963e2e77080fac8f2ee252b71eb1aecbd3825c2
wisehackermonkey <oranbusiness@gmail.com>
2019-03-03 22:50:37 -0800

    setup p5.js and empty magic-eye.js canvas that works with

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+ magic eye rendered game
+ by Oran C
+ oranbusiness@gmail.com
+ github.com/wisehackermonkey
+ 20190303
+*/
+let MagicEye = require("magic-eye");
+magicEye = new MagicEye();
+magicEye.el = "magic-eye";
+magicEye.width = 500;
+magicEye.height = 400;
+magicEye.render();
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+	createCanvas(400, 400);
+	background(50);
+	let magicEye = new MagicEye({ el: "magic-eye", width: 500, height: 400 }).render();
+}
+
+function draw() {
+	
+}
