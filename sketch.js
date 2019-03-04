0211daf842ee8d88f0cd968b79629b5ba65fbd71
wisehackermonkey <oranbusiness@gmail.com>
2019-03-03 23:55:18 -0800

    working movement

--- sketch.js
+++ sketch.js
@@ -10,20 +10,32 @@
  https://www.npmjs.com/package/magic-eye
 */
 let game_board = [];
+let pos;
+var magicEye;
 
 function setup() {
     createCanvas(400, 400);
     background(50);
+    frameRate(10);
+    pos = createVector(0, 2);
 
     create_game_board(5,5);
-    game_board[0][0] = 1;
-    game_board[1][1] = 1;
-    game_board[2][2] = 1;
-
-    render_magic_eye(game_board);
 }
 
 function draw() {
+    if (keyIsDown(RIGHT_ARROW)) {
+        pos.x += 1;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        pos.x -= 1;
+    }
+    // try {
+    game_board[pos.y][pos.x] = 1;
+    // }catch (e) {
+    //     console.error(` out of bound :${e}`);
+    // }
+
+    render_magic_eye(game_board);
 
 }
 
@@ -41,6 +53,16 @@ Array.matrix = function(numrows, numcols, initial){
     }
     return arr;
 }
+
+// transpose(matrix)
+// https://stackoverflow.com/a/13241545/5460870
+p5.prototype.transpose = function(a) {
+    return Object.keys(a[0]).map(function(c) {
+        return a.map(function(r) {
+            return r[c];
+        });
+    });
+}
 function create_game_board(width,height) {
     let board = Array.matrix(width,height,0);
     game_board = board || [
@@ -57,5 +79,9 @@ function render_magic_eye(depth_map) {
         [0, 1, 0],
         [0, 0, 0]
     ];
-    new MagicEye({el: "magic-eye",depthMap:depth_map, width: 500, height: 400,}).render();
+    let colors = [ [255, 0, 0, 125],
+        [0, 255, 0, 255],
+        [0, 0, 255, 125] ];;
+    magicEye = new MagicEye({el: "magic-eye",depthMap:depth_map,palette:colors, width: 500, height: 400,}).render();
+    // magicEye.generatePalette(4);
 }
